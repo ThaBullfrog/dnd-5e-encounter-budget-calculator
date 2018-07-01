a033d36924f8096068ec3eeb8a5513c98fe5dd38
ThaBullfrog <th3bullfrog@gmail.com>
2018-07-01 02:29:05 -0400

    install css-library

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,11 +9,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [{ 
-      test: /\.js$/, 
-      exclude: /node_modules/, 
-      loader: "babel-loader" 
-    }]
-  }
+    rules: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel-loader" 
+      },
+			{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+	plugins: [ 
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
 };
 

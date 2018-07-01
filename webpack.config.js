ebdb67455861fbdc501a4010290e67a019f6b31e
ThaBullfrog <th3bullfrog@gmail.com>
2018-07-01 01:16:53 -0400

    install and configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{ 
+      test: /\.js$/, 
+      exclude: /node_modules/, 
+      loader: "babel-loader" 
+    }]
   }
 };
 

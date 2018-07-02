93601a6b4d9c0d45c5eb11dcad767cc5ab6ffb8e
ThaBullfrog <th3bullfrog@gmail.com>
2018-07-01 22:17:39 -0400

    add calculator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './scss/main.scss';
 import './navbar';
 import './on-scroll-animations';
+import EncounterData from './EncounterData';
+import numberWithCommas from './numberWithCommas';
+
+const playersInput = document.querySelector('#players');
+const levelInput = document.querySelector('#level');
+const enemiesInput = document.querySelector('#enemies');
+const outputs = {
+  easy: document.querySelector('#easy'),
+  medium: document.querySelector('#medium'),
+  hard: document.querySelector('#hard'),
+  deadly: document.querySelector('#deadly')
+};
+const budgets = {}
+
+function updateOutput() {
+  let players = parseInt(playersInput.value);
+  let level = parseInt(levelInput.value);
+  let enemies = parseInt(enemiesInput.value);
+  if (level >= 1 && level <= 20) {
+    EncounterData.difficulties.forEach(diff => {
+      let threshold = EncounterData.thresholds[level - 1][diff];
+      let multiplier = EncounterData.multiplier(enemies);
+      let budget = threshold * players / multiplier;
+      outputs[diff].innerHTML = numberWithCommas(budget);
+    });
+  }
+}
+
+updateOutput();
+
+playersInput.addEventListener('input', updateOutput);
+levelInput.addEventListener('input', updateOutput);
+enemiesInput.addEventListener('input', updateOutput);
--- src/on-scroll-animations.js
+++ src/on-scroll-animations.js
@@ -1,10 +1,10 @@
 const animationElements = document.querySelectorAll('.animated');
 
 if (animationElements.length > 0) {
-  animationElements.forEach((e) => e.classList.add('dont-animate-yet'));
+  animationElements.forEach(e => e.classList.add('dont-animate-yet'));
 
   function checkIfInView() {
-    animationElements.forEach((element) => {
+    animationElements.forEach(element => {
       let top = element.getBoundingClientRect().top;
       console.log(top);
       if (top + element.offsetHeight * 0.5 <= window.innerHeight) {
